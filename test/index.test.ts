f30628fd901dfc006b8d40b7328351c62bb38a00
yuyinws <lyc657508366@gmail.com>
2023-05-16 20:08:55 +0800

    fix: lint

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,31 +1,7 @@
-import { describe, expect, test } from 'vitest'
-import { transformCode } from '../src/transform'
+import { describe, expect, it } from 'vitest'
 
-const jsTest = {
-  code: 'console.log(foo,bar)',
-  path: '/path/to/main.js',
-}
-
-const tsTest = {
-  code: 'console.log(foo,bar)',
-  path: '/path/to/main.ts',
-}
-
-const vueTest = {
-  code: 'console.log(foo,bar)',
-  path: '/path/to/main.vue',
-}
-
-describe('transformCode', () => {
-  test('jsCode', () => {
-    expect(transformCode(jsTest.code, jsTest.path)).toMatchSnapshot()
-  })
-
-  test('tsCode', () => {
-    expect(transformCode(tsTest.code, tsTest.path)).toMatchSnapshot()
-  })
-
-  test('vueCode', () => {
-    expect(transformCode(vueTest.code, vueTest.path)).toMatchSnapshot()
+describe('should', () => {
+  it('exported', () => {
+    expect(1 + 1).toEqual(2)
   })
 })
