c816f5a711350567db2ca2ea1fe9f81b2257446a
yuyinws <lyc657508366@gmail.com>
2023-12-01 11:35:08 +0800

    chore: clean code

--- src/dir.ts
+++
@@ -1,8 +0,0 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const DIR_DIST = typeof __dirname !== 'undefined'
-  ? __dirname
-  : dirname(fileURLToPath(import.meta.url))
-
-export const DIR_CLIENT = resolve(DIR_DIST, './client')
