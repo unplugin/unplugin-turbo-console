96e031f81a9aa6bf99e68e00c518a1bd35a599c5
yuyinws <lyc657508366@gmail.com>
2024-04-04 16:45:05 +0800

    refactor: serveStatic

--- src/core/dir.ts
+++ src/core/dir.ts
@@ -5,5 +5,4 @@ export const DIR_DIST = typeof __dirname !== 'undefined'
   ? __dirname
   : dirname(fileURLToPath(import.meta.url))
 
-export const DIR_CLIENT = resolve(DIR_DIST, './client/index.html')
-export const DIR_INTRO = resolve(DIR_DIST, './client/intro.html')
+export const CLIENT_DIR = resolve(DIR_DIST, './client')
