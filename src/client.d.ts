20a4f458e336ace57f12799148bf9fcb16d95b11
yuyinws <lyc657508366@gmail.com>
2024-07-09 15:51:47 +0800

    fix: client types

--- src/client.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '~console' {
-  export const initWebSocket: () => void
-  export const tConsole: {
-    log: (...args: any[]) => void
-    error: (...args: any[]) => void
-    warn: (...args: any[]) => void
-    info: (...args: any[]) => void
-  }
-}
