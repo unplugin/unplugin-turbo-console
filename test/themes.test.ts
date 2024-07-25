feb555c74991377a0e30df9c9dd26f066bce7d39
yuyinws <lyc657508366@gmail.com>
2024-07-25 15:37:43 +0800

    chore: expose theme utils

--- test/themes.test.ts
+++ test/themes.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from 'vitest'
-import { getStyleCode } from '../src/core/utils/themes'
+import { getStyleCode } from '../src/core/utils'
 
 describe('themes', () => {
   it('get style code', () => {
