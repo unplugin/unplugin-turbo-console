e6c332767edf38004fcff5a47af0ff8a27bb77e4
yuyinws <lyc657508366@gmail.com>
2024-07-25 15:32:39 +0800

    refactor: utils

--- test/themes.test.ts
+++ test/themes.test.ts
@@ -3,6 +3,11 @@ import { getStyleCode } from '../src/core/utils/themes'
 
 describe('themes', () => {
   it('get style code', () => {
-    expect(getStyleCode('vue')).toMatchInlineSnapshot(`"padding:2px 5px;border-radius:3px 0 0 3px;margin:0 0 5px 0;color:#fff;background:#4FC08D;"`)
+    expect(getStyleCode('vue')).toMatchInlineSnapshot(`
+      {
+        "highlight": "padding:2px 5px;border-radius:3px 0 0 3px;margin:0 0 5px 0;color:#fff;background:#4FC08D;",
+        "launchEditor": "background:#00DC8250;padding:2px 5px;border-radius:0 3px 3px 0;margin:0 0 5px 0;",
+      }
+    `)
   })
 })
