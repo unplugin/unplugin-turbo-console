6d5291e4be52aeb548e6f167ce45e033a2595d99
yuyinws <lyc657508366@gmail.com>
2024-08-13 15:45:00 +0800

    chore: update deps

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,11 @@
 /* eslint-disable no-var */
 /* eslint-disable vars-on-top */
-import type { Peer } from 'crossws'
 
 export interface global {}
 
 declare global {
+  import type { Peer } from 'crossws'
+
   var TurboConsoleFilePathMap: Map<string, string>
   var UNPLUGIN_TURBO_CONSOLE_LAUNCH_SERVER: boolean
   var UNPLUGIN_TURBO_CONSOLE_PEERS_SET: Set<Peer> | undefined
