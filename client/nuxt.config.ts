951c9ef942f672b6031db49855d2d0e60fa82331
yuyinws <lyc657508366@gmail.com>
2025-03-05 14:24:12 +0800

    chore: monorepo for client app

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -5,12 +5,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   ssr: false,
-  srcDir: 'app',
+  future: {
+    compatibilityVersion: 4,
+  },
   css: ['./app/assets/main.css'],
   nitro: {
     preset: 'static',
     output: {
-      dir: 'dist/client',
+      dir: '../dist/client',
     },
   },
   modules: ['reka-ui/nuxt'],
