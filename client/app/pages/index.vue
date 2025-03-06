b1dc74aeab33fd79104eaa129448aea372088fbe
yuyinws <lyc657508366@gmail.com>
2025-03-06 13:43:19 +0800

    chore: try to fix build

--- client/app/pages/index.vue
+++ client/app/pages/index.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="">
 // ; (async () => {
 //   try {
 //     const position = window.location.hash.slice(1)
---
+++ client/app/pages/inspect.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { data, error } = await useAsyncData(async () => {
+  const raw = await fetch(`/filePathMap`)
+  const response = await raw.json()
+  return response
+})
+</script>
+
+<template>
+  <Suspense>
+    <template #default>
+      <div v-if="error" class="text-red-500">
+        {{ data }}
+        {{ error }}
+      </div>
+    </template>
+    <template #fallback>
+      Loading...
+    </template>
+  </Suspense>
+</template>
